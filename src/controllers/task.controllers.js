909c4fe23556217e4c1d56f0b0adcdf8152a48e5
Vaibhav2024 <vaibhavpatil090604@gmail.com>
2026-02-13 12:11:33 +0530

    Writing Task Controllers

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -10,15 +10,159 @@ import { Task } from "../models/task.models.js";
 import { Subtask } from "../models/subtask.models.js";
 
 const getTasks = asyncHandler(async(req, res) => {
-    // test
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new ApiError(404, "Project not found")
+    }
+
+    const tasks = await Task.find({ //remember we are using find so it will find all task related to that project and return array
+        project: new mongoose.Types.ObjectId(projectId)
+    }).populate("assignedTo", "avatar username fullName") // there is always no need to use aggregation pipeline for small task so we use populate which takes the field from you forign table that is refering to another model and then you can simply tell what you want from that model, it return an array
+    /* 
+        {
+            "_id": "task1",
+            "title": "Fix bug",
+            "assignedTo": {
+                "_id": "65fd3b2c9f123abc12345678",
+                "avatar": "avatar.png",
+                "username": "john_doe",
+                "fullName": "John Doe"
+            }
+        }
+    */
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            tasks,
+            "Tasks fetched Successfully"
+        )
+    )
 });
 
 const getTaskById = asyncHandler(async(req, res) => {
-    // test
+    const {taskId} = req.params;
+    const task = await Task.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(taskId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "assignedTo",
+                foreignField: "_id",
+                as: "assignedTo",
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 1,
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "subtasks",
+                localField: "_id",
+                foreignField: "task",
+                as: "subtasks",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "createdBy",
+                            foreignField: "_id",
+                            as: "createdBy",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        _id: 1,
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            createdBy: {
+                                $arrayElemAt: ["$createdBy", 0]
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                assignedTo: {
+                    $arrayElemAt: ["$assignedTo", 0]
+                }
+            }
+        }
+    ]);
+
+    if (!task || task.length === 0) {
+        throw new ApiError(404, "Task not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            task[0],
+            "task fetched successfully"
+        )
+    )
 });
 
 const createTask = asyncHandler(async(req, res) => {
-    // test
+    const {title, description, assignedTo, status} = req.body;
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    const files = req.files || [];
+
+    const attachments = files.map((file) => {
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+            mimetype: file.mimetype,
+            size: file.size
+        }
+    })
+
+    const task = await Task.create({
+        title,
+        description,
+        project: new mongoose.Types.ObjectId(projectId),
+        assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo) : undefined,
+        status,
+        assignedBy: new mongoose.Types.ObjectId(req.user._id),
+        attachments
+    })
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            task,
+            "Task Created Successfully"
+        )
+    )
 });
 
 const updateTask = asyncHandler(async(req, res) => {
